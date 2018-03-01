21a043912a44753b7703b39f6962b9da35f9fee9
itagn <2361305184@qq.com>
2018-03-01 13:51:15 +0800

    reset code

--- src/vue-iclock.vue
+++ src/vue-iclock.vue
@@ -46,41 +46,47 @@
       }
     },
     created(){
-      this.scale = +this.display.scale || 1;
-      this.language = this.display.language || 'en';
-      this.emoji = this.display.emoji || 'smile';
-      this.className = this.display.className || '';
-      this.glasses = this.display.glasses || false;
-      this.font = {
-        type: this.display.type || 'clock',
-        info: this.display.info || 'o w o',
-        fontSize: this.display.fontSize || '1.5rem',
-        fontColor: this.display.fontColor || 'orange',
-        fontStyle: this.display.fontStyle || "Helvetica,'Microsoft YaHei'",
-        dateColor: this.display.dateColor || '#999'
-      }
+      this.initData();
     },
     mounted() {
-      var clock = document.querySelector(this.className + " .iclock");
-      this.dom = document.querySelector(this.className + " .iclock .iclock-show");
-
-      clock.style.transform = "scale("+this.scale+")";
-      clock.style.webkitTransform = "scale("+this.scale+")";
-
-      this.dom.style.color = this.font.fontColor;
-      this.dom.style.fontSize = this.font.fontSize;
-      this.dom.style.fontFamily = this.font.fontStyle;
-
       this.init();
     },
     methods: {
+      initData(){
+        this.scale = +this.display.scale || 1;
+        this.language = this.display.language || 'en';
+        this.emoji = this.display.emoji || 'smile';
+        this.className = this.display.className || '';
+        this.glasses = this.display.glasses || false;
+        this.font = {
+          type: this.display.type || 'clock',
+          info: this.display.info || 'o w o',
+          fontSize: this.display.fontSize || '1.5rem',
+          fontColor: this.display.fontColor || 'orange',
+          fontStyle: this.display.fontStyle || "Helvetica,'Microsoft YaHei'",
+          dateColor: this.display.dateColor || '#999'
+        }
+      },
       init(){
+        this.initStyle();
+        this.checkType();
+        this.checkEmoji();
+      },
+      initStyle(){
+        var clock = document.querySelector(this.className + " .iclock");
+        this.dom = document.querySelector(this.className + " .iclock .iclock-show");
+        clock.style.transform = "scale("+this.scale+")";
+        clock.style.webkitTransform = "scale("+this.scale+")";
+        this.dom.style.color = this.font.fontColor;
+        this.dom.style.fontSize = this.font.fontSize;
+        this.dom.style.fontFamily = this.font.fontStyle;
+      },
+      checkType(){
         if(this.font.type === 'clock'){
           var date = document.querySelector(this.className + " .iclock .iclock-date");
           var week = document.querySelector(this.className + " .iclock .iclock-week");
           date.style.color = this.font.dateColor;
           week.style.color = this.font.dateColor;
-
           this.week = this.getDate().week;
           this.date = this.getDate().date;
           this.show = this.getDate().time;
@@ -90,26 +96,15 @@
         } else {
           this.errTip('Error: props[display].type should be "clock" or "text".');
         }
+      },
+      checkEmoji(){
         var mouse = document.querySelector(this.className + " .iclock .iclock-body .iclock-mouse");
         if(this.emoji === 'smile'){
-          mouse.style.borderTop = '80px solid #ccc';
-          mouse.style.borderLeft = '80px solid transparent';
-          mouse.style.borderRight = '80px solid transparent';
+          this.smile(mouse);
         } else if (this.emoji === 'angry'){
-          mouse.style.border = '40px solid #d53a35';
-          mouse.style.borderRadius = '10%';
+          this.angry(mouse);
         } else if (this.emoji === 'jiong'){
-          var leftEye = document.querySelector(this.className + " .iclock .iclock-body .iclock-left-eyes");
-          var rightEye = document.querySelector(this.className + " .iclock .iclock-body .iclock-right-eyes");
-          leftEye.style.transform = 'rotate(-10deg)';
-          leftEye.style.webkitTransform = 'rotate(-10deg)';
-          rightEye.style.transform = 'rotate(10deg)';
-          rightEye.style.webkitTransform = 'rotate(10deg)';
-
-          mouse.style.border = '40px solid #e98f6f';
-          mouse.style.borderRadius = '10%';
-          mouse.style.left = '56px';
-          mouse.style.width = '10px';
+          this.jiong(mouse);
         } else {
           this.errTip('Error: props[display].emoji should be "smile" or "angry".');
         }
@@ -127,7 +122,6 @@
       },
       getDate(){
         var dates = new Date();
-
         var h = dates.getHours()+'';
         h = h.length === 2 ? h : '0'+h;
         var m = dates.getMinutes()+'';
@@ -135,7 +129,6 @@
         var s = dates.getSeconds()+'';
         s = s.length === 2 ? s : '0'+s;
         var time = h+':'+m+':'+s;
-
         var y = dates.getFullYear();
         var m = dates.getMonth() + 1;
         var d = dates.getDate();
@@ -151,6 +144,27 @@
           date = m+'/'+d+'/'+y;
         }
         return { date, time, week };
+      },
+      smile(mouse){
+        mouse.style.borderTop = '80px solid #ccc';
+        mouse.style.borderLeft = '80px solid transparent';
+        mouse.style.borderRight = '80px solid transparent';
+      },
+      angry(mouse){
+        mouse.style.border = '40px solid #d53a35';
+        mouse.style.borderRadius = '10%';
+      },
+      jiong(mouse){
+        var leftEye = document.querySelector(this.className + " .iclock .iclock-body .iclock-left-eyes");
+        var rightEye = document.querySelector(this.className + " .iclock .iclock-body .iclock-right-eyes");
+        leftEye.style.transform = 'rotate(-10deg)';
+        leftEye.style.webkitTransform = 'rotate(-10deg)';
+        rightEye.style.transform = 'rotate(10deg)';
+        rightEye.style.webkitTransform = 'rotate(10deg)';
+        mouse.style.border = '40px solid #e98f6f';
+        mouse.style.borderRadius = '10%';
+        mouse.style.left = '56px';
+        mouse.style.width = '10px';
       }
     }
   }
